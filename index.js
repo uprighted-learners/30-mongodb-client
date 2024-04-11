6329d1c568ffb61071e5632e0e8a00f766c486b8
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-04-11 18:31:39 -0500

    get users

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ const { MongoClient } = require('mongodb');
 // init express app
 const app = express();
 
+// parse json body
+app.use(express.json());
+
 // declare a port
 const PORT = process.env.PORT || 8080;
+
+// declare a mongo uri
 const mongoURI = process.env.MONGO_URI;
 
+// init mongodb client
 let client = new MongoClient(mongoURI);
 
 // connect to mongodb
@@ -25,8 +31,30 @@ async function dbConnect() {
     }
 }
 
-dbConnect();
+// POST - /api/create - create a new user
+app.post('/api/create', async (req, res) => {
+    let user = req.body;
+    let userColl = await dbConnect();
+    await userColl.insertOne(user);
+    client.close();
+    res.send(user);
+});
 
+// GET - /api/userdata - get all users
+app.get('/api/userdata', async (req, res) => {
+    let userColl = await dbConnect();
+    let results = [];
+
+    let userList = await userColl.find({});
+
+    await userList.forEach(userObj => {
+        results.push(userObj);
+    })
+
+    res.json(results);
+});
+
+// GET - /api/health - tells us if server is alive
 app.get('/api/health', (req, res) => {
     res.send('OK');
 });
